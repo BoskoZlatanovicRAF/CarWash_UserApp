6147bde3715084bd15b06e4b599c08c1f53bac80
BoskoZlatanovicRAF <bzlatanovic3621rn@raf.rs>
2024-12-01 00:27:16 +0100

    added Admin BottomNavigationBar

--- app/src/main/java/com/example/payten_windowsxp_userapp/Navigation/bottomBar/BottomNavigationBar.kt
+++ app/src/main/java/com/example/payten_windowsxp_userapp/Navigation/bottomBar/BottomNavigationBar.kt
@@ -9,25 +9,44 @@ import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.produceState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.Navigator
+import com.example.payten_windowsxp_userapp.Users.RoleEnum
+import com.example.payten_windowsxp_userapp.auth.AuthStore
 import java.util.Locale
 
 @Composable
-fun BottomNavigationBar(navController: NavController, currentRoute: String?) {
-    val screens = listOf(
-        Screen.Home,
-        Screen.Location,
-        Screen.QR,
-        Screen.Notifications,
-        Screen.Profile
-    )
+fun BottomNavigationBar(
+    navController: NavController,
+    currentRoute: String?
+) {
+    val viewModel: BottomNavigationViewModel = hiltViewModel()
+    val userRole by viewModel.userRole.collectAsState()
+
+    val screens = when (userRole) {
+        RoleEnum.ADMIN -> listOf(
+            Screen.AdminHome,
+            Screen.AdminProfile
+        )
+        else -> listOf(
+            Screen.Home,
+            Screen.Location,
+            Screen.QR,
+            Screen.Notifications,
+            Screen.Profile
+        )
+    }
 
     NavigationBar(
         containerColor = Color(0xFF333333),
@@ -45,7 +64,8 @@ fun BottomNavigationBar(navController: NavController, currentRoute: String?) {
                 icon = {
                     when (screen) {
                         is Screen.VectorScreen -> Icon(
-                            imageVector = if (currentRoute == screen.route) screen.selectedIcon else screen.unselectedIcon,
+                            imageVector = if (currentRoute == screen.route) screen.selectedIcon
+                            else screen.unselectedIcon,
                             contentDescription = screen.route
                         )
                         is Screen.DrawableScreen -> {
@@ -54,14 +74,20 @@ fun BottomNavigationBar(navController: NavController, currentRoute: String?) {
                                     modifier = Modifier.padding(vertical = 8.dp)
                                 ) {
                                     Icon(
-                                        painter = painterResource(if (currentRoute == screen.route) screen.selectedIcon else screen.unselectedIcon),
+                                        painter = painterResource(
+                                            if (currentRoute == screen.route) screen.selectedIcon
+                                            else screen.unselectedIcon
+                                        ),
                                         contentDescription = screen.route,
                                         modifier = Modifier.size(45.dp)
                                     )
                                 }
                             } else {
                                 Icon(
-                                    painter = painterResource(if (currentRoute == screen.route) screen.selectedIcon else screen.unselectedIcon),
+                                    painter = painterResource(
+                                        if (currentRoute == screen.route) screen.selectedIcon
+                                        else screen.unselectedIcon
+                                    ),
                                     contentDescription = screen.route
                                 )
                             }
@@ -69,6 +95,15 @@ fun BottomNavigationBar(navController: NavController, currentRoute: String?) {
                     }
                 }
             )
+        }
+    }
+}
+
+@Composable
+fun rememberUserRole(authStore: AuthStore): State<RoleEnum> {
+    return produceState(initialValue = RoleEnum.USER) {
+        authStore.authData.collect { authData ->
+            value = authData.role
         }
     }
 }
