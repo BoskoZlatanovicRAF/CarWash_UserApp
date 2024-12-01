82006b60dbd8e3abd679d8d60c86f190379dc6d6
BoskoZlatanovicRAF <bzlatanovic3621rn@raf.rs>
2024-12-01 02:19:28 +0100

    nearest car wash station

--- app/src/main/java/com/example/payten_windowsxp_userapp/Users/user/userhomescreen/UserHomeScreenContract.kt
+++ app/src/main/java/com/example/payten_windowsxp_userapp/Users/user/userhomescreen/UserHomeScreenContract.kt
@@ -1,6 +1,8 @@
 package com.example.payten_windowsxp_userapp.Users.user.userhomescreen
 
+import android.location.Location
 import com.example.payten_windowsxp_userapp.Users.user.db.Transaction
+import com.example.payten_windowsxp_userapp.Users.user.locationScreen.CarWashLocation
 
 interface UserHomeScreenContract {
 
@@ -8,10 +10,14 @@ interface UserHomeScreenContract {
         val loading: Boolean = false,
         val name: String = "",
         val bonusPoints: Long = 0,
-        val transactions: List<Transaction> = emptyList()
+        val transactions: List<Transaction> = emptyList(),
+        val nearestCarWash: CarWashLocation? = null,
+        val distanceToNearestCarWash: Float = 0f // u metrima
     )
 
-    sealed class UserHomeScreenEvent {
 
+
+    sealed class UserHomeScreenEvent {
+        data class UpdateCurrentLocation(val latitude: Double, val longitude: Double) : UserHomeScreenEvent()
     }
 }
